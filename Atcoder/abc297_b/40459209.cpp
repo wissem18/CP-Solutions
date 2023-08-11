318745b00a1e6ea97e9a472afd8ecc9b6e4c9335
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 13:41:27 +0100

    Committing files

---
+++ Atcoder/abc297_b/40459209.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    int l=0,r=0;
+    z[0]=n;
+    for (int i = 1; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);[
+    //freopen("output.txt", "w", stdout);
+int n,d;
+cin>>n>>d;
+int t[n];
+    for (int i = 0; i < n; ++i) {
+        cin>>t[i];
+    }
+    int ans=-1;
+    for (int i = 1; i <n ; ++i) {
+        if(t[i]-t[i-1]<=d){
+            ans=t[i];
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
