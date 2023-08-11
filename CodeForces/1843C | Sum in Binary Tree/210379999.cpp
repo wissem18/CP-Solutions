f0b01eb108c3d3444737f486378ab67208cd568e
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:01:30 +0100

    Committing files

---
+++ CodeForces/1843C | Sum in Binary Tree/210379999.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int MOD = 1e9 + 7;
+ll a(ll n){
+    if(n==1)
+        return 1;
+    return a(n/2)+n;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    // freopen("triangles.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll ans=a(n);
+        cout<<ans<<endl;
+    }
+  }
