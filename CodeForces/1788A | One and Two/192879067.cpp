cbeb1bd79bb5957d02b7e59bbe0b1f807e6e500e
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:39:03 +0100

    Committing files

---
+++ CodeForces/1788A | One and Two/192879067.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+     int t;
+     cin>>t;
+     while(t--){
+         int n;
+         cin>>n;
+         int a[n];
+         int pre[n+1];
+         pre[0]=0;
+         for (int i = 0; i < n; ++i) {
+             cin>>a[i];
+             pre[i+1]=pre[i]+(a[i]==2);
+         }
+         int ans=-1;
+         for (int i = 1; i <=n ; ++i) {
+             if(pre[i]==pre[n]-pre[i]){
+                 ans=i;
+                 break;
+             }
+         }
+         cout<<ans<<nl;
+     }
+}
