9dd5b61d207b878ba2b1afac230edbda6b2866d0
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 20:37:29 +0100

    Committing files

---
+++ CodeForces/1772A | A+B?/191717930.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+ int t;
+ cin>>t;
+ while(t--) {
+     string s;
+     cin>>s;
+     int ans=s[0]-'0'+s[2]-'0';
+     cout<<ans<<nl;
+ }
+}
