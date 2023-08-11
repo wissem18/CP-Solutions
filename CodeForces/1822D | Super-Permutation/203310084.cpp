7ab177e978b373168819e79b2fe157080fe9027d
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:32:37 +0100

    Committing files

---
+++ CodeForces/1822D | Super-Permutation/203310084.cpp
@@ -0,0 +1,60 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+int MOD = 1e9 + 7;
+int lastStoneWeight(vector<int>& stones) {
+priority_queue<int>q;
+    for (auto x:stones) {
+     q.push(x);
+    }
+while(q.size()>1){
+    int x=q.top();
+    q.pop();
+    int y=q.top();
+    q.pop();
+    if(x!=y){
+        q.push(x-y);
+    }
+}
+if(q.empty())
+    return 0;
+return q.top();
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+ int t;
+ cin>>t;
+ while(t--){
+int n;
+cin>>n;
+if(n==1)
+    cout<<1<<endl;
+else{
+    if(n%2){
+        cout<<-1<<endl;
+    }
+    else{
+        vector<int>a;
+        for (int i = 0; i < n/2; ++i) {
+            a.push_back(i);
+            a.push_back(n-i-1);
+        }
+        cout<<n<<" ";
+        for (int i = 0; i < n-1; ++i) {
+            cout<<(a[i]-a[i+1]+n)%n<<" ";
+        }
+        cout<<endl;
+    }
+}
+ }
+}
