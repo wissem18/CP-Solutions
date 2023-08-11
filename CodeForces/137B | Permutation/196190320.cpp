59e65a0337bfde3a830091d0a9be1dbbdd3b448c
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:35:49 +0100

    Committing files

---
+++ CodeForces/137B | Permutation/196190320.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9+7;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int n;
+    cin>>n;
+    int  a[n];
+    vector<bool>vis(5001,false);
+    for (int i = 0; i < n; ++i) {
+        cin>>a[i];
+        vis[a[i]]=true;
+    }
+   int ans=0;
+    for (int i = 1; i <=n ; ++i) {
+        if(!vis[i])
+            ans++;
+    }
+    cout<<ans;
+}
