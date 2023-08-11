e43f32d5012100cde75b2a223e1ffd84c3070a74
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:31:13 +0100

    Committing files

---
+++ CodeForces/1840A | Cipher Shifer/208837535.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    // freopen("triangles.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string ans="";
+    int i=0;
+    char cur=s[0];
+    while(i<n){
+        int j=i+1;
+        while(j<n&&s[j]!=cur)
+            j++;
+        ans+=cur;
+        j++;
+       cur=s[j];
+       i=j;
+    }
+    cout<<ans<<endl;
+}
+}
