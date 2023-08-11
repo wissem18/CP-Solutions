a7d6241d7854c3e5156e48e37c2d5086a7a7a0bb
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:37:55 +0100

    Committing files

---
+++ CodeForces/1051B | Relatively Prime Pairs/193925273.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ll l,r;
+    cin>>l>>r;
+    cout<<"YES"<<nl;
+    for (ll i = l; i <=r ; i+=2) {
+        cout<<i<<" "<<i+1<<nl;
+    }
+}
