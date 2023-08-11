4b6a57d0ac9a953ddfb146f835309d5bca1996e0
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:33:28 +0100

    Committing files

---
+++ CodeForces/1814A | Coins/201004156.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        ll n,k;
+        cin>>n>>k;
+        if(n%2&&k%2==0)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+ 
+    }
+}
