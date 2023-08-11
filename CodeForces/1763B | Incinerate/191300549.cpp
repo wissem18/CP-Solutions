1cb0253df682b37aa753152fbed84e1a2a586cbc
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 20:37:55 +0100

    Committing files

---
+++ CodeForces/1763B | Incinerate/191300549.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+string intToBinary(int n)
+{
+    string r;
+    while(n!=0) {r=(n%2==0 ?"0":"1")+r; n/=2;}
+    return r;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int h[n],p[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>h[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            cin>>p[i];
+        }
+        int mx=*max_element(h,h+n);
+        pi a[n];
+        for (int i = 0; i < n; ++i) {
+            a[i]={p[i],h[i]};
+        }
+        sort(a,a+n);
+        ll cum=0;
+        bool test=true;
+        for(int i=0;i<n;i++){
+            if(a[i].second<=cum)
+                continue;
+ 
+            if(i>0)
+            k-=a[i].first;
+            if(k<0){
+                test= false;
+                break;
+            }
+            cum+=k;
+            while(cum<a[i].second){
+                k-=a[i].first;
+                if(k<0){
+                    test= false;
+                    break;
+                }
+                cum+=k;
+            }
+        }
+        (test)?cout<<"YES"<<nl:cout<<"NO"<<nl;
+    }
+}
