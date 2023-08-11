06e43cfddf4eac993aac1719498c78f0d3a89047
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:34:00 +0100

    Committing files

---
+++ CodeForces/1810D | Climbing the Tree/200036845.cpp
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+const int N=1e5+5;
+map<int,ll> mem;
+ll dp(int i,int lst,int n,ll &c,ll &d,vector<int>&z){
+    if(i==n)
+        return 0;
+    if(mem.count(i))
+        return mem[i];
+return mem[i]=min(dp(i+1,z[i],n,c,d,z)+d*1LL*(z[i]-lst-1),dp(n,z[i],n,c,d,z)+c*1LL*(n-i));
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+  int t;
+  cin>>t;
+  while(t--){
+     int q;
+     cin>>q;
+     ll hmn=0,hmx=1e18;
+     while(q--){
+         int type;
+         cin>>type;
+         if(type==1){
+             int a,b,n;
+             cin>>b>>a>>n;
+ 
+             ll newhmx=(b-a)*1LL*(n-1)+b;
+             ll newhmn=newhmx-(b-a)+1;
+             if(n==1){
+                  newhmx=b;
+                  newhmn=1;
+             }
+             if((newhmx<hmn||newhmn>hmx)&&hmn!=0)
+                 cout<<0<<" ";
+             else
+             {
+                 cout<<1<<" ";
+                 hmx=min(hmx,newhmx);
+                 hmn=max(hmn,newhmn);
+             }
+         }
+         else{
+            int a,b;
+            cin>>a>>b;
+            ll x1=(hmx-a>0)?(hmx-a)/(a-b)+((hmx-a)%(a-b)!=0)+1:1;
+            ll x2=(hmn-a>0)?(hmn-a)/(a-b)+((hmn-a)%(a-b)!=0)+1:1;
+            if(x1==x2)
+                cout<<x1<<" ";
+            else
+                cout<<-1<<" ";
+         }
+     }
+     cout<<endl;
+  }
+}
