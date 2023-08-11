ffbe190a6abc207f7c8a991a02111637e7de2ce6
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:05:30 +0100

    Committing files

---
+++ CodeForces/1836A | Destroyer/210111204.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    // freopen("triangles.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--) {
+   int n;
+   cin>>n;
+   int a[n];
+   vector<int>occ(105,0);
+        for (int i = 0; i < n; ++i) {
+            cin>>a[i];
+            occ[a[i]]++;
+        }
+   bool test=true;
+        for (int i = 1; i <100 ; ++i) {
+            if(occ[i]>occ[i-1]){
+                test=false;
+                break;
+            }
+        }
+        (test)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+    }
+}
