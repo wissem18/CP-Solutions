eae5281dd57b12f2c4c943442caaf2c45bc2b721
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 13:42:54 +0100

    Committing files

---
+++ Atcoder/abc287_a/38378289.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int n;
+    cin>>n;
+    string a[n];
+    int cnt=0;
+    for (int i = 0; i < n; ++i) {
+        cin>>a[i];
+        if(a[i]=="For")
+            cnt++;
+    }
+    if(cnt>n/2)
+        cout<<"Yes"<<nl;
+    else
+        cout<<"No"<<nl;
+}
