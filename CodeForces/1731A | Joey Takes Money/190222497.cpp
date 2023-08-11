1a379334e1d58cbef882f4f00e9cac8326f714cc
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 20:39:09 +0100

    Committing files

---
+++ CodeForces/1731A | Joey Takes Money/190222497.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD =1e9+7;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+   int t;
+   cin>>t;
+   while(t--){
+       int n;
+       cin>>n;
+       ll a[n];
+       ll p=1;
+       for (int i = 0; i < n; ++i) {
+           cin>>a[i];
+           p*=a[i];
+       }
+       p+=n-1;
+       p*=2022;
+       cout<<p<<nl;
+   }
+}
