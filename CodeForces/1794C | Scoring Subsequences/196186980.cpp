60a53f8cbd69db0c138bb57356a03a61aa1acd91
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:35:52 +0100

    Committing files

---
+++ CodeForces/1794C | Scoring Subsequences/196186980.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9+7;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>a[i];
+        }
+        for (int i = 0; i < n; ++i) {
+          int l=1,r=i+1;
+          while(l<=r){
+              int mid=(l+r)/2;
+              if(a[i-mid+1]<mid)
+                  r=mid-1;
+              else
+                  l=mid+1;
+          }
+          cout<<r<<" ";
+        }
+        cout<<nl;
+    }
+}
