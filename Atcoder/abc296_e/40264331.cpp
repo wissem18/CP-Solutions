4931cfba0d946d5715aa28bdf4d5e487b9ca451f
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 13:41:34 +0100

    Committing files

---
+++ Atcoder/abc296_e/40264331.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+int n;
+cin>>n;
+int x;
+cin>>x;
+int a[n];
+map<int,int>occ;
+    for (int i = 0; i < n; ++i) {
+        cin>>a[i];
+        occ[a[i]]++;
+    }
+    bool test=false;
+    for (int i = 0; i < n; ++i) {
+
+            if(occ[a[i]+x]>=1){
+                test=true;
+                break;
+            }
+    }
+    (test)?cout<<"Yes":cout<<"No";
+}
