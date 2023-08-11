ad3317daa4b07f5aa1c4d7d3ecc396e43f69e664
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 20:37:00 +0100

    Committing files

---
+++ CodeForces/1791A | Codeforces Checking/191856678.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+const char nl = '\n';
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+  int t;
+  cin>>t;
+  string s="codeforces";
+  while(t--){
+      char c;
+      cin>>c;
+      bool test=false;
+      for (int i = 0; i < s.size(); ++i) {
+          if(s[i]==c){
+              test= true;
+              break;
+          }
+      }
+      if(test)
+          cout<<"YES"<<nl;
+      else
+          cout<<"NO"<<nl;
+  }
+}
