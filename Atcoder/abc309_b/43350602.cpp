58a8d5f9d076b95642c4134d4c825b9197ade450
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 13:40:51 +0100

    Committing files

---
+++ Atcoder/abc309_b/43350602.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+double const pi = 3.141592653;
+vector<int> dx = {0, 1, 0, -1};
+vector<int> dy = {1, 0, -1, 0};
+int MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    // freopen("triangles.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+int a,b;
+cin>>a>>b;
+if(b-a==1&&a%3)
+    cout<<"Yes"<<endl;
+else
+    cout<<"No"<<endl;
+}
+
