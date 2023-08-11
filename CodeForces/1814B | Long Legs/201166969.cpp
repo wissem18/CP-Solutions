5e38661705f2236b9a675656ac66b68dce7a4b97
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:33:22 +0100

    Committing files

---
+++ CodeForces/1814B | Long Legs/201166969.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+        swap(a,b);
+    int ans=1e9;
+    for (int i = 1; i<=1e5; ++i) {
+        ans=min(ans,i-1+(a+i-1)/i+(b+i-1)/i);
+    }
+    cout<<ans<<endl;
+}
+}
