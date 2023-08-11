47462e2599eeaf649548497c7969464f87070f56
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-11 15:05:41 +0100

    Committing files

---
+++ CodeForces/1834D | Survey in Class/210095455.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+#define ll long long
+//#define pi pair<int,int>
+#define pll pair<ll,ll>
+//double const pi= 3.141592653;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int MOD = 1e9 + 7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    // freopen("triangles.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int l[n], r[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> l[i] >> r[i];
+        }
+        int mxl = 0, mnr = m,mnlen=m,mxlen=0;
+        for (int i = 0; i < n; ++i) {
+            mxl = max(mxl, l[i]);
+            mnr = min(mnr, r[i]);
+            mnlen=min(mnlen,r[i]-l[i]+1);
+            mxlen=max(mxlen,r[i]-l[i]+1);
+        }
+ 
+        int ans = mxlen-mnlen;//the minimum answer because if we ask the student with max length the student with min length can't respond more than
+        //his length which they are in the same interval of the max interval
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, min(r[i] - l[i] + 1, max(mxl - l[i], r[i] - mnr)));
+        }
+        cout << ans * 2 << endl;
+    }
+}
